cccde357e4c15ad7a2d6b6956d1c72ff8fc0efaa
chiranjit278 <78475327+chiranjit278@users.noreply.github.com>
2022-03-19 20:05:32 +0530

    dthfhgfftf

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function myfunction(){
+    let plus=  document.getElementById("demo").style.fontSize = "20px";
+}
+function myfunction1(){
+    let plus=  document.getElementById("demo").style.fontSize = "17px";
+  }
+  function myfunction2(){
+    let plus=  document.getElementById("demo").style.fontSize = "15px";
+  }
+  
